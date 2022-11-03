17fb17508db6406cd8f9c42d9fe61688302e616b
seetoe <kevinseetoe@gmail.com>
2022-11-02 23:02:55 -0400

    initial commit

---
+++ build/precache-manifest.5191110b63973da5d9e7f7fc33179a8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2feef3ab692acda54f91a815b25487",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6642ef91997dec3759d3",
+    "url": "/static/css/main.cbd5b20e.chunk.css"
+  },
+  {
+    "revision": "07f942897c0829b8b866",
+    "url": "/static/js/2.aa1cf97d.chunk.js"
+  },
+  {
+    "revision": "6642ef91997dec3759d3",
+    "url": "/static/js/main.3b056854.chunk.js"
+  },
+  {
+    "revision": "51003887673d2242fbc3",
+    "url": "/static/js/runtime-main.6a33371b.js"
+  }
+]);
